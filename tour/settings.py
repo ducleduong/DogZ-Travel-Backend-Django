d9368d59ec406626c476bf5cd71a25e740f2c5c3
ducleduong <duc.leduongg@gmail.com>
2021-08-09 22:24:45 +0700

    catch error

--- tour/settings.py
+++ tour/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
 MEDIA_ROOT = '%s/tourbooking/static' % BASE_DIR
 CKEDITOR_UPLOAD_PATH = 'images/'
 
@@ -137,6 +137,7 @@ CKEDITOR_UPLOAD_PATH = 'images/'
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
